a081f9e2ff2ba8a6ea5b79c18b7cb58c9999f98f
Jeremy Soller <jackpot51@gmail.com>
2018-03-10 16:03:21 -0700

    Remove IRQ debug print

--- rtl8168d/src/main.rs
+++ rtl8168d/src/main.rs
@@ -59,8 +59,6 @@ fn main() {
                 if isr != 0 {
                     irq_file.write(&mut irq)?;
 
-                    print!("{}", format!("rtl8168d: IRQ: {:X}\n", isr));
-
                     let mut todo = todo_irq.borrow_mut();
                     let mut i = 0;
                     while i < todo.len() {
