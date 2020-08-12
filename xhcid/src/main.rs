b9d41e86917c4b0449c7d56dea4a68de506b5365
4lDO2 <4lDO2@protonmail.com>
2020-08-12 15:22:53 +0200

    Less logs, and remove path dependency.

--- xhcid/src/main.rs
+++ xhcid/src/main.rs
@@ -39,21 +39,8 @@ fn setup_logging() -> Option<&'static RedoxLogger> {
         .with_process_name("xhcid".into())
         .with_output(
             OutputBuilder::stderr()
-                .with_filter(log::LevelFilter::Debug) // limit global output to important info
+                .with_filter(log::LevelFilter::Info) // limit global output to important info
                 .with_ansi_escape_codes()
-                .flush_on_newline(true)
-                .build()
-        )
-        .with_output(
-            OutputBuilder::with_endpoint(std::fs::OpenOptions::new()
-                .create_new(false)
-                .read(false)
-                .write(true)
-                .open("debug:").unwrap()
-            )
-                .with_ansi_escape_codes()
-                //.with_filter(log::LevelFilter::Trace)
-                .with_filter(log::LevelFilter::Debug)
                 .flush_on_newline(true)
                 .build()
         );
@@ -62,7 +49,7 @@ fn setup_logging() -> Option<&'static RedoxLogger> {
     match OutputBuilder::in_redox_logging_scheme("usb", "host", "xhci.log") {
         Ok(b) => logger = logger.with_output(
             // TODO: Add a configuration file for this
-            b.with_filter(log::LevelFilter::Debug)
+            b.with_filter(log::LevelFilter::Info)
                 .flush_on_newline(true)
                 .build()
         ),
@@ -72,7 +59,7 @@ fn setup_logging() -> Option<&'static RedoxLogger> {
     #[cfg(target_os = "redox")]
     match OutputBuilder::in_redox_logging_scheme("usb", "host", "xhci.ansi.log") {
         Ok(b) => logger = logger.with_output(
-            b.with_filter(log::LevelFilter::Debug)
+            b.with_filter(log::LevelFilter::Info)
                 .with_ansi_escape_codes()
                 .flush_on_newline(true)
                 .build()
