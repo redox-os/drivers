f750d4223d8c6d2641579a5cb8f38e1c701f0852
Anhad Singh <andypython@protonmail.com>
2023-06-19 12:19:57 +1000

    virtio: perform the device reset
    Signed-off-by: Anhad Singh <andypython@protonmail.com>

--- virtiod/src/lib.rs
+++ virtiod/src/lib.rs
@@ -1,6 +1,95 @@
 //! https://docs.oasis-open.org/virtio/virtio/v1.1/virtio-v1.1.html
 
+use core::cell::UnsafeCell;
 use static_assertions::const_assert_eq;
+
+#[derive(Debug, Copy, Clone)]
+#[repr(u8)]
+pub enum CfgType {
+    /// Common Configuration.
+    Common = 1,
+    /// Notifications.
+    Notify = 2,
+    /// ISR Status.
+    Isr = 3,
+    /// Device specific configuration.
+    Device = 4,
+    /// PCI configuration access.
+    PciConfig = 5,
+    /// Shared memory region.
+    SharedMemory = 8,
+    /// Vendor-specific data.
+    Vendor = 9,
+}
+
+const_assert_eq!(core::mem::size_of::<CfgType>(), 1);
+
+#[derive(Debug, Copy, Clone)]
+#[repr(C, packed)]
+pub struct PciCapability {
+    /// Identifies the structure.
+    pub cfg_type: CfgType,
+    /// Where to find it.
+    pub bar: u8,
+    /// Pad to a full dword.
+    pub padding: [u8; 3],
+    /// Offset within the bar.
+    pub offset: u32,
+    /// Length of the structure, in bytes.
+    pub length: u32,
+}
+
+// The size of `PciCapability` is 13 bytes since
+// the generic PCI fields are *not* included.
+const_assert_eq!(core::mem::size_of::<PciCapability>(), 13);
+
+bitflags::bitflags! {
+    #[derive(Debug, Copy, Clone, PartialEq)]
+    #[repr(transparent)]
+    pub struct DeviceStatusFlags: u8 {
+        /// Indicates that the guest OS has found the device and recognized it as a
+        /// valid device.
+        const ACKNOWLEDGE = 1;
+        /// Indicates that the guest OS knows how to drive the device.
+        const DRIVER = 2;
+        /// Indicates that something went wrong in the guest and it has given up on
+        /// the device.
+        const FAILED = 128;
+        /// Indicates that the driver has acknowledged all the features it understands
+        /// and feature negotiation is complete.
+        const FEATURES_OK = 8;
+        /// Indicates that the driver is set up and ready to drive the device.
+        const DRIVER_OK = 4;
+        /// Indicates that the device has experienced an error from which it can’t recover.
+        const DEVICE_NEEDS_RESET = 64;
+    }
+}
+
+#[derive(Debug)]
+#[repr(C)]
+pub struct CommonCfg {
+    // About the whole device.
+    pub device_feature_select: VolatileCell<u32>, // read-write
+    pub device_feature: VolatileCell<u32>,        // read-only for driver
+    pub driver_feature_select: VolatileCell<u32>, // read-write
+    pub driver_feature: VolatileCell<u32>,        // read-write
+    pub msix_config: VolatileCell<u16>,           // read-write
+    pub num_queues: VolatileCell<u16>,            // read-only for driver
+    pub device_status: VolatileCell<DeviceStatusFlags>, // read-write
+    pub config_generation: VolatileCell<u8>,      // read-only for driver
+
+    // About a specific virtqueue.
+    pub queue_select: VolatileCell<u16>,      // read-write
+    pub queue_size: VolatileCell<u16>,        // read-write
+    pub queue_msix_vector: VolatileCell<u16>, // read-write
+    pub queue_enable: VolatileCell<u16>,      // read-write
+    pub queue_notify_off: VolatileCell<u16>,  // read-only for driver
+    pub queue_desc: VolatileCell<u64>,        // read-write
+    pub queue_driver: VolatileCell<u64>,      // read-write
+    pub queue_device: VolatileCell<u64>,      // read-write
+}
+
+const_assert_eq!(core::mem::size_of::<CommonCfg>(), 56);
 
 bitflags::bitflags! {
     #[repr(transparent)]
@@ -15,7 +104,7 @@ bitflags::bitflags! {
 }
 
 #[repr(C)]
-struct Descriptor {
+pub struct Descriptor {
     /// Address (guest-physical).
     address: u64,
     /// Size of the descriptor.
@@ -29,6 +118,30 @@ const_assert_eq!(core::mem::size_of::<Descriptor>(), 16);
 
 pub struct VirtQueue {}
 
-pub struct Transport {}
+pub struct StandardTransport {}
 
+impl StandardTransport {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
 
+#[derive(Debug)]
+#[repr(transparent)]
+pub struct VolatileCell<T> {
+    value: UnsafeCell<T>,
+}
+
+impl<T: Copy> VolatileCell<T> {
+    /// Returns a copy of the contained value.
+    #[inline]
+    pub fn get(&self) -> T {
+        unsafe { core::ptr::read_volatile(self.value.get()) }
+    }
+
+    /// Sets the contained value.
+    #[inline]
+    pub fn set(&self, value: T) {
+        unsafe { core::ptr::write_volatile(self.value.get(), value) }
+    }
+}
--- virtiod/src/main.rs
+++ virtiod/src/main.rs
@@ -1,10 +1,26 @@
-use pcid_interface::{PciFeature, PcidServerHandle};
+use pcid_interface::{Capability, PciBar, PcidServerHandle};
+use thiserror::Error;
+use virtiod::*;
+
+use syscall::{PHYSMAP_NO_CACHE, PHYSMAP_WRITE};
+
+// TODO(andypython):
+//
+//  cc 3.1.1 Driver Requirements: Device Initialization
+// Reset the device.
+// Set the ACKNOWLEDGE status bit: the guest OS has noticed the device.
+// Set the DRIVER status bit: the guest OS knows how to drive the device.
+// Read device feature bits, and write the subset of feature bits understood by the OS and driver to the device. During this step the driver MAY read (but MUST NOT write) the device-specific configuration fields to check that it can support the device before accepting it.
+// Set the FEATURES_OK status bit. The driver MUST NOT accept new feature bits after this step.
+// Re-read device status to ensure the FEATURES_OK bit is still set: otherwise, the device does not support our subset of features and the device is unusable.
+// Perform device-specific setup, including discovery of virtqueues for the device, optional per-bus setup, reading and possibly writing the device’s virtio configuration space, and population of virtqueues.
+// Set the DRIVER_OK status bit. At this point the device is “live”.
 
 pub fn main() -> anyhow::Result<()> {
     #[cfg(target_os = "redox")]
     setup_logging();
 
-    redox_daemon::Daemon::new(daemon).expect("virtio-core: failed to daemonize");
+    redox_daemon::Daemon::new(daemon_runner).expect("virtio-core: failed to daemonize");
 }
 
 #[cfg(target_os = "redox")]
@@ -49,24 +65,123 @@ fn setup_logging() {
     log::info!("virtiod: enabled logger");
 }
 
-fn daemon(_deamon: redox_daemon::Daemon) -> ! {
-    let mut pcid_handle = PcidServerHandle::connect_default().unwrap();
-    let pci_config = pcid_handle.fetch_config().unwrap();
+#[derive(Debug, Copy, Clone, Error)]
+enum Error {
+    #[error("capability {0:?} not found")]
+    InCapable(CfgType),
+    #[error("failed to map memory")]
+    MapErr,
+}
+
+fn deamon(_deamon: redox_daemon::Daemon) -> anyhow::Result<()> {
+    let mut pcid_handle = PcidServerHandle::connect_default()?;
+    let pci_config = pcid_handle.fetch_config()?;
+    let pci_header = pcid_handle.fetch_header()?;
 
     // 0x1001 - virtio-blk
     assert_eq!(pci_config.func.devid, 0x1001);
     log::info!("virtiod: found `virtio-blk` device");
 
-    // Get the PCI capabilities.
-    // for vendor_cap in pcid_handle
-    //     .fetch_all_features()
-    //     .unwrap()
-    //     .iter()
-    //     .filter(|(x, _)| matches!(x, PciFeature::VendorSpecific))
-    // {
+    let mut common_addr = None;
+    let mut notify_addr = None;
+    let mut isr_addr = None;
+    let mut device_addr = None;
 
-    // }
-    // log::info!("{}", x.unwrap() as u8);
+    for capability in pcid_handle
+        .get_capabilities()?
+        .iter()
+        .filter_map(|capability| {
+            if let Capability::Vendor(vendor) = capability {
+                Some(vendor)
+            } else {
+                None
+            }
+        })
+    {
+        assert!(capability.data.len() >= core::mem::size_of::<PciCapability>());
 
+        // SAFETY: We have verified that the length of the data is correct.
+        let capability = unsafe { &*(capability.data.as_ptr() as *const PciCapability) };
+
+        match capability.cfg_type {
+            CfgType::Common | CfgType::Notify | CfgType::Isr | CfgType::Device => {}
+            _ => continue,
+        }
+
+        let bar = pci_header.get_bar(capability.bar as usize);
+        let addr = match bar {
+            PciBar::Memory32(addr) => addr as usize,
+            PciBar::Memory64(addr) => addr as usize,
+
+            _ => unreachable!("virtio: unsupported bar type: {bar:?}"),
+        };
+
+        let address = unsafe {
+            syscall::physmap(
+                addr + capability.offset as usize,
+                capability.length as usize,
+                PHYSMAP_WRITE | PHYSMAP_NO_CACHE,
+            )
+            .map_err(|_| Error::MapErr)?
+        };
+
+        match capability.cfg_type {
+            CfgType::Common => {
+                debug_assert!(common_addr.is_none());
+                common_addr = Some(address);
+            }
+
+            CfgType::Notify => {
+                debug_assert!(notify_addr.is_none());
+                notify_addr = Some(address);
+            }
+
+            CfgType::Isr => {
+                debug_assert!(isr_addr.is_none());
+                isr_addr = Some(address);
+            }
+
+            CfgType::Device => {
+                debug_assert!(device_addr.is_none());
+                device_addr = Some(address);
+            }
+
+            _ => unreachable!(),
+        }
+
+        log::info!("virtio: {capability:?}");
+    }
+
+    let common_addr = common_addr.ok_or(Error::InCapable(CfgType::Common))?;
+    let notify_addr = notify_addr.ok_or(Error::InCapable(CfgType::Notify))?;
+    let isr_addr = isr_addr.ok_or(Error::InCapable(CfgType::Isr))?;
+    let device_addr = device_addr.ok_or(Error::InCapable(CfgType::Device))?;
+
+    let common = unsafe { &mut *(common_addr as *mut CommonCfg) };
+
+    // Reset the device.
+    common.device_status.set(DeviceStatusFlags::empty());
+    // Upon reset, the device must initialize device status to 0.
+    assert_eq!(common.device_status.get(), DeviceStatusFlags::empty());
+    log::info!("virtio: successfully reseted the device");
+
+    // XXX: According to the virtio specification v1.2, setting the ACKNOWLEDGE and DRIVER bits
+    //      in `device_status` are required to be done in two steps.
+    common
+        .device_status
+        .set(common.device_status.get() | DeviceStatusFlags::ACKNOWLEDGE);
+
+    common
+        .device_status
+        .set(common.device_status.get() | DeviceStatusFlags::DRIVER);
+
+    log::info!("virtio: using standard PCI transport");
+
+    let _transport = StandardTransport::new();
     loop {}
+}
+
+fn daemon_runner(redox_daemon: redox_daemon::Daemon) -> ! {
+    deamon(redox_daemon).unwrap();
+    unreachable!();
 }
