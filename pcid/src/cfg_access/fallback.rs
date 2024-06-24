e7781dcced8a23aa48ec9454c6c4d447fdf27b81
Jeremy Soller <jackpot51@gmail.com>
2024-06-24 11:43:17 -0600

    Fix compilation on aarch64

--- pcid/src/cfg_access/fallback.rs
+++ pcid/src/cfg_access/fallback.rs
@@ -84,10 +84,6 @@ impl ConfigRegionAccess for Pci {
 }
 #[cfg(not(any(target_arch = "x86", target_arch = "x86_64")))]
 impl ConfigRegionAccess for Pci {
-    fn function_exists(&self, _address: PciAddress) -> bool {
-        todo!();
-    }
-
     unsafe fn read(&self, addr: PciAddress, offset: u16) -> u32 {
         let _guard = self.lock.lock().unwrap();
         todo!("Pci::CfgAccess::read on this architecture")
--- virtio-core/src/arch/aarch64.rs
+++ virtio-core/src/arch/aarch64.rs
@@ -4,6 +4,6 @@ use pcid_interface::*;
 
 use crate::{transport::Error, Device};
 
-pub fn enable_msix(pcid_handle: &mut PcidServerHandle) -> Result<File, Error> {
+pub fn enable_msix(pcid_handle: &mut PciFunctionHandle) -> Result<File, Error> {
     unimplemented!("virtio_core: aarch64 enable_msix")
 }
