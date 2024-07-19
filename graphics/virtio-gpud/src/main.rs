8a91bae0a69ebf9df8d85876277c315a551a9e1e
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-07-19 21:25:52 +0200

    Remove unnecessary unsafe block

--- graphics/virtio-gpud/src/main.rs
+++ graphics/virtio-gpud/src/main.rs
@@ -453,9 +453,7 @@ fn deamon(deamon: redox_daemon::Daemon) -> anyhow::Result<()> {
         socket_file
             .read(&mut packet)
             .expect("virtio-gpud: failed to read disk scheme");
-        unsafe {
-            scheme.handle(&mut packet);
-        }
+        scheme.handle(&mut packet);
         socket_file
             .write(&packet)
             .expect("virtio-gpud: failed to read disk scheme");
