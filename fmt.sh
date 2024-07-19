1a41bcab14352e67042a03c1a3c57058542a8bfd
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-07-19 21:39:58 +0200

    Enforce formatting in CI

--- fmt.sh
+++ fmt.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/bash
 
+set -eo pipefail
+
 function fmt() {
     for dir in "$@"
     do
         pushd $dir
         printf "\e[1;32mFormatting\e[0m $dir\n"
-        cargo fmt
+        if [[ "$CHECK_ONLY" -eq "1" ]]; then
+            cargo fmt --check
+        else
+            cargo fmt
+        fi
         popd
     done
 }
 
-fmt virtio-core \
-    virtio-netd \
-    virtio-blkd \
-    virtio-gpud \
-    inputd
+fmt graphics/bgad \
+    graphics/fbcond \
+    graphics/vesad \
+    graphics/virtio-gpud \
+    inputd \
+    net/virtio-netd \
+    virtio-core
