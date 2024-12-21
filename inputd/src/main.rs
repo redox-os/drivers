8e92e2c74387663d3fae2499e974b7c1bf6f3ad6
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2024-12-21 18:09:15 +0100

    inputd: Let graphics drivers pull vt activation events from inputd
    Previously inputd would directly push vt activation events to the
    graphics driver, which required being quite lazy to prevent deadlocks as
    well as the graphics driver having a location where events can be pushed
    to. By having graphics drivers pull the vt activation events instead,
    the effective control flow becomes simpler and it becomes easier to
    correctly handle multiple graphics drivers on the system. For example it
    becomes possible for multiple graphics drivers to present displays for a
    single VT as well as making it easier to provide a handoff from the
    early framebuffer to a real graphics driver.

--- inputd/src/main.rs
+++ inputd/src/main.rs
@@ -11,11 +11,12 @@
 //! Read events from `input:consumer`. Optionally, set the `EVENT_READ` flag to be notified when
 //! events are available.
 
+use core::mem::size_of;
 use std::collections::BTreeMap;
-use std::fs::File;
+use std::mem::transmute;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-use inputd::{Cmd, VtActivate};
+use inputd::{VtActivate, VtEvent, VtEventKind};
 
 use redox_scheme::{RequestKind, SchemeMut, SignalBehavior, Socket, V2};
 
@@ -31,6 +32,9 @@ enum Handle {
         vt: usize,
     },
     Display {
+        events: EventFlags,
+        pending: Vec<VtEvent>,
+        notified: bool,
         device: String,
     },
     Control,
@@ -44,27 +48,13 @@ impl Handle {
 
 struct Vt {
     display: String,
-    index: usize,
-
-    /// This is *required* to be lazily initialized since opening the handle to the display
-    /// requires the system call to return first. Otherwise, it will block indefinitely.
-    handle_file: Option<File>,
 }
 
 impl Vt {
-    fn new(display: impl Into<String>, index: usize) -> Self {
+    fn new(display: impl Into<String>) -> Self {
         Self {
             display: display.into(),
-            handle_file: None,
-            index,
         }
-    }
-
-    fn send_command(&mut self, cmd: Cmd) -> Result<(), libredox::error::Error> {
-        let handle_file = self
-            .handle_file
-            .get_or_insert_with(|| File::open(format!("/scheme/{}/handle", self.display)).unwrap());
-        inputd::send_comand(handle_file, cmd)
     }
 }
 
@@ -78,7 +68,6 @@ struct InputScheme {
     super_key: bool,
     active_vt: Option<usize>,
 
-    pending_activate: Option<VtActivate>,
     has_new_events: bool,
 }
 
@@ -93,9 +82,66 @@ impl InputScheme {
             super_key: false,
             active_vt: None,
 
-            pending_activate: None,
             has_new_events: false,
         }
+    }
+
+    fn switch_vt(&mut self, new_active: usize) -> syscall::Result<()> {
+        if let Some(active_vt) = self.active_vt {
+            if new_active == active_vt {
+                return Ok(());
+            }
+        }
+
+        if !self.vts.contains_key(&new_active) {
+            log::warn!("inputd: switch to non-existent VT #{new_active} was requested");
+            return Ok(());
+        }
+
+        log::info!(
+            "inputd: switching from VT #{} to VT #{new_active}",
+            self.active_vt.unwrap_or(0)
+        );
+
+        for handle in self.handles.values_mut() {
+            match handle {
+                Handle::Display {
+                    pending,
+                    notified,
+                    device,
+                    ..
+                } => {
+                    if let Some(active_vt) = self.active_vt {
+                        if &self.vts[&active_vt].display == &*device {
+                            pending.push(VtEvent {
+                                kind: VtEventKind::Deactivate,
+                                vt: self.active_vt.unwrap(),
+                                width: 0,
+                                height: 0,
+                                stride: 0,
+                            });
+                            *notified = false;
+                        }
+                    }
+
+                    if &self.vts[&new_active].display == &*device {
+                        pending.push(VtEvent {
+                            kind: VtEventKind::Activate,
+                            vt: new_active,
+                            width: 0,
+                            height: 0,
+                            stride: 0,
+                        });
+                        *notified = false;
+                    }
+                }
+                _ => continue,
+            }
+        }
+
+        self.active_vt = Some(new_active);
+
+        Ok(())
     }
 }
 
@@ -123,7 +169,12 @@ impl SchemeMut for InputScheme {
             }
             "handle" => {
                 let display = path_parts.collect::<Vec<_>>().join(".");
-                Handle::Display { device: display }
+                Handle::Display {
+                    events: EventFlags::empty(),
+                    pending: Vec::new(),
+                    notified: false,
+                    device: display,
+                }
             }
             "control" => Handle::Control,
 
@@ -175,12 +226,32 @@ impl SchemeMut for InputScheme {
                 Ok(copy)
             }
 
-            Handle::Display { device } => {
-                assert!(buf.is_empty());
+            Handle::Display {
+                pending, device, ..
+            } => {
+                // FIXME Create new VT through a write instead and return a NewVt event on read
+                // This allows also returning events for VT (de)activation from the display handle
+                // rather than pushing them to the graphics driver.
+                if buf.is_empty() {
+                    // Trying to do an empty read creates a new VT.
+                    let vt = self.next_vt_id.fetch_add(1, Ordering::SeqCst);
+                    log::info!("inputd: created VT #{vt} for {device}");
+                    self.vts.insert(vt, Vt::new(device.clone()));
+                    Ok(vt)
+                } else if buf.len() % size_of::<VtEvent>() == 0 {
+                    let copy = core::cmp::min(pending.len(), buf.len() / size_of::<VtEvent>());
 
-                let vt = self.next_vt_id.fetch_add(1, Ordering::SeqCst);
-                self.vts.insert(vt, Vt::new(device.clone(), vt));
-                Ok(vt)
+                    for (i, event) in pending.drain(..copy).enumerate() {
+                        buf[i * size_of::<VtEvent>()..(i + 1) * size_of::<VtEvent>()]
+                            .copy_from_slice(&unsafe {
+                                transmute::<VtEvent, [u8; size_of::<VtEvent>()]>(event)
+                            });
+                    }
+                    Ok(copy * size_of::<VtEvent>())
+                } else {
+                    log::error!("inputd: display tried to read incorrectly sized event");
+                    return Err(SysError::new(EINVAL));
+                }
             }
 
             Handle::Producer => {
@@ -207,7 +278,7 @@ impl SchemeMut for InputScheme {
 
         match handle {
             Handle::Control => {
-                if buf.len() != core::mem::size_of::<VtActivate>() {
+                if buf.len() != size_of::<VtActivate>() {
                     log::error!("inputd: control tried to write incorrectly sized command");
                     return Err(SysError::new(EINVAL));
                 }
@@ -215,7 +286,7 @@ impl SchemeMut for InputScheme {
                 // SAFETY: We have verified the size of the buffer above.
                 let cmd = unsafe { &*buf.as_ptr().cast::<VtActivate>() };
 
-                self.pending_activate = Some(cmd.clone());
+                self.switch_vt(cmd.vt)?;
 
                 return Ok(buf.len());
             }
@@ -238,11 +309,11 @@ impl SchemeMut for InputScheme {
         let events = unsafe {
             core::slice::from_raw_parts(
                 buf.as_ptr() as *const Event,
-                buf.len() / core::mem::size_of::<Event>(),
+                buf.len() / size_of::<Event>(),
             )
         };
 
-        'out: for event in events.iter() {
+        for event in events.iter() {
             let mut new_active_opt = None;
             match event.to_option() {
                 EventOption::Key(key_event) => match key_event.scancode {
@@ -270,42 +341,41 @@ impl SchemeMut for InputScheme {
                 },
 
                 EventOption::Resize(resize_event) => {
-                    let active_vt = self.vts.get_mut(&self.active_vt.unwrap()).unwrap();
-                    active_vt.send_command(Cmd::Resize {
-                        vt: active_vt.index,
-                        width: resize_event.width,
-                        height: resize_event.height,
+                    for handle in self.handles.values_mut() {
+                        match handle {
+                            Handle::Display {
+                                pending,
+                                notified,
+                                device,
+                                ..
+                            } => {
+                                if &self.vts[&self.active_vt.unwrap()].display == &*device {
+                                    pending.push(VtEvent {
+                                        kind: VtEventKind::Resize,
+                                        vt: self.active_vt.unwrap(),
+                                        width: resize_event.width,
+                                        height: resize_event.height,
 
-                        // TODO(andypython): Figure out how to get the stride.
-                        stride: resize_event.width,
-                    })?;
+                                        // TODO(andypython): Figure out how to get the stride.
+                                        stride: resize_event.width,
+                                    });
+                                    *notified = false;
+                                }
+                            }
+                            _ => continue,
+                        }
+                    }
                 }
 
                 _ => continue,
             }
 
             if let Some(new_active) = new_active_opt {
-                if new_active == self.vts[&self.active_vt.unwrap()].index {
-                    continue 'out;
-                }
-
-                if self.vts.contains_key(&new_active) {
-                    let active_vt = self.vts.get_mut(&self.active_vt.unwrap()).unwrap();
-
-                    active_vt.send_command(Cmd::Deactivate(active_vt.index))?;
-                }
-
-                if let Some(new_active) = self.vts.get_mut(&new_active) {
-                    new_active.send_command(Cmd::Activate {
-                        vt: new_active.index,
-                    })?;
-                    self.active_vt = Some(new_active.index);
-                } else {
-                    log::warn!("inputd: switch to non-existent VT #{new_active} was requested");
-                }
+                self.switch_vt(new_active)?;
             }
         }
 
+        let handle = self.handles.get_mut(&id).ok_or(SysError::new(EINVAL))?;
         assert!(handle.is_producer());
 
         let active_vt = self.active_vt.unwrap();
@@ -348,8 +418,17 @@ impl SchemeMut for InputScheme {
                 *notified = false;
                 Ok(EventFlags::empty())
             }
-            Handle::Producer | Handle::Control | Handle::Display { .. } => {
-                log::error!("inputd: producer, control or display tried to use an event queue");
+            Handle::Display {
+                ref mut events,
+                ref mut notified,
+                ..
+            } => {
+                *events = flags;
+                *notified = false;
+                Ok(EventFlags::empty())
+            }
+            Handle::Producer | Handle::Control => {
+                log::error!("inputd: producer or control tried to use an event queue");
                 Err(SysError::new(EINVAL))
             }
         }
@@ -385,47 +464,51 @@ fn deamon(deamon: redox_daemon::Daemon) -> anyhow::Result<()> {
             RequestKind::MsyncMsg | RequestKind::MunmapMsg | RequestKind::MmapMsg => unreachable!(),
         }
 
-        if let Some(cmd) = scheme.pending_activate.take() {
-            if let Some(vt) = scheme.vts.get_mut(&cmd.vt) {
-                // Failing to activate a VT is not a fatal error.
-                if let Err(err) = vt.send_command(Cmd::Activate { vt: vt.index }) {
-                    log::error!("inputd: failed to activate VT #{}: {err}", vt.index)
-                }
-
-                scheme.active_vt = Some(vt.index);
-            } else {
-                log::error!("inputd: failed to activate non-existent VT #{}", cmd.vt)
-            }
-        }
-
         if !scheme.has_new_events {
             continue;
         }
 
         for (id, handle) in scheme.handles.iter_mut() {
-            if let Handle::Consumer {
-                events,
-                pending,
-                ref mut notified,
-                vt,
-            } = handle
-            {
-                if pending.is_empty() || *notified || !events.contains(EventFlags::EVENT_READ) {
-                    continue;
+            match handle {
+                Handle::Consumer {
+                    events,
+                    pending,
+                    ref mut notified,
+                    vt,
+                } => {
+                    if pending.is_empty() || *notified || !events.contains(EventFlags::EVENT_READ) {
+                        continue;
+                    }
+
+                    let active_vt = scheme.active_vt.unwrap();
+
+                    // The activate VT is not the same as the VT that the consumer is listening to
+                    // for events.
+                    if active_vt != *vt {
+                        continue;
+                    }
+
+                    // Notify the consumer that we have some events to read. Yum yum.
+                    socket_file.post_fevent(*id, EventFlags::EVENT_READ.bits())?;
+
+                    *notified = true;
                 }
+                Handle::Display {
+                    events,
+                    pending,
+                    ref mut notified,
+                    ..
+                } => {
+                    if pending.is_empty() || *notified || !events.contains(EventFlags::EVENT_READ) {
+                        continue;
+                    }
 
-                let active_vt = scheme.active_vt.unwrap();
+                    // Notify the consumer that we have some events to read. Yum yum.
+                    socket_file.post_fevent(*id, EventFlags::EVENT_READ.bits())?;
 
-                // The activate VT is not the same as the VT that the consumer is listening to
-                // for events.
-                if active_vt != *vt {
-                    continue;
+                    *notified = true;
                 }
-
-                // Notify the consumer that we have some events to read. Yum yum.
-                socket_file.post_fevent(*id, EventFlags::EVENT_READ.bits())?;
-
-                *notified = true;
+                _ => {}
             }
         }
     }
