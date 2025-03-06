398103e7103e732437764904106fc104e375b105
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-03-06 22:18:21 +0100

    storage/partitionlib: Rustfmt

--- storage/partitionlib/tests/test.rs
+++ storage/partitionlib/tests/test.rs
@@ -27,28 +27,34 @@ fn part_is_mbr() {
 // NOTE: The following tests rely on outside resource files being correct.
 #[test]
 fn gpt() {
-    let table = get_partitions_from_file("./resources/disk.img", LogicalBlockSize::Lb512).unwrap().unwrap();
-    assert_eq!(&table.partitions, &[
-        Partition {
+    let table = get_partitions_from_file("./resources/disk.img", LogicalBlockSize::Lb512)
+        .unwrap()
+        .unwrap();
+    assert_eq!(
+        &table.partitions,
+        &[Partition {
             flags: Some(0),
             name: Some("bug".to_owned()),
             uuid: Some(uuid::Uuid::parse_str("b665fba9-74d5-4069-a6b9-5ba3a164fdfe").unwrap()), // Microsoft basic data
             size: 957,
             start_lba: 34,
-        }
-    ]);
+        }]
+    );
 }
 
 #[test]
 fn mbr() {
-    let table = get_partitions_from_file("./resources/disk_mbr.img", LogicalBlockSize::Lb512).unwrap().unwrap();
-    assert_eq!(&table.partitions, &[
-        Partition {
+    let table = get_partitions_from_file("./resources/disk_mbr.img", LogicalBlockSize::Lb512)
+        .unwrap()
+        .unwrap();
+    assert_eq!(
+        &table.partitions,
+        &[Partition {
             flags: None,
             name: None,
             uuid: None,
             size: 3,
             start_lba: 1,
-        }
-    ]);
+        }]
+    );
 }
