f7540f24f63754dde41b190b21ec4622bacb1be5
Jeremy Soller <jackpot51@gmail.com>
2025-10-18 17:53:51 -0600

    hwd: describe more ACPI devices

--- hwd/src/backend/acpi.rs
+++ hwd/src/backend/acpi.rs
@@ -58,11 +58,17 @@ impl Backend for AcpiBackend {
                     };
                     let what = match id.as_str() {
                         // https://uefi.org/specs/ACPI/6.5/05_ACPI_Software_Programming_Model.html
-                        "ACPI0006" => "GPE block device",
-                        "ACPI0010" => "Processor control device",
+                        "ACPI0003" => "Power source",
+                        "ACPI0006" => "GPE block",
+                        "ACPI0007" => "Processor",
+                        "ACPI0010" => "Processor control",
                         // https://uefi.org/sites/default/files/resources/devids%20%285%29.txt
+                        "PNP0000" => "AT interrupt controller",
+                        "PNP0100" => "AT timer",
                         "PNP0103" => "HPET",
+                        "PNP0200" => "AT DMA controller",
                         "PNP0303" => "IBM Enhanced (101/102-key, PS/2 mouse support)",
+                        "PNP030B" => "PS/2 keyboard",
                         "PNP0400" => "Standard LPT printer port",
                         "PNP0501" => "16550A-compatible COM port",
                         "PNP0A03" | "PNP0A08" => "PCI bus",
@@ -70,7 +76,16 @@ impl Backend for AcpiBackend {
                         "PNP0A06" => "Generic ACPI Extended-IO bus (EIO bus)",
                         "PNP0B00" => "AT real-time clock",
                         "PNP0C01" => "System board",
-                        "PNP0C0F" => "PCI interrupt link device",
+                        "PNP0C02" => "Reserved resources",
+                        "PNP0C04" => "Math coprocessor",
+                        "PNP0C09" => "Embedded controller",
+                        "PNP0C0A" => "Battery",
+                        "PNP0C0B" => "Fan",
+                        "PNP0C0C" => "Power button",
+                        "PNP0C0D" => "Lid sensor",
+                        "PNP0C0E" => "Sleep button",
+                        "PNP0C0F" => "PCI interrupt link",
+                        "PNP0C50" => "I2C HID",
                         "PNP0F13" => "PS/2 port for PS/2-style mouse",
                         _ => "?",
                     };
