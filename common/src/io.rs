9ff6340b933c3f4054eeba09072cebac02f04869
Jeremy Soller <jackpot51@gmail.com>
2025-12-01 21:37:15 -0700

    common: do not allow writef when WriteOnly

--- common/src/io.rs
+++ common/src/io.rs
@@ -91,9 +91,5 @@ impl<I: Io> WriteOnly<I> {
         self.inner.write(value)
     }
 
-    #[inline(always)]
-    /// Calls [Io::writef]
-    pub fn writef(&mut self, flags: I::Value, value: bool) {
-        self.inner.writef(flags, value)
-    }
+    // writef requires read which is not valid when write-only
 }
